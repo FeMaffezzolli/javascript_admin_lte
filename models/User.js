f2e100a2fa8315482ea064eb3532dd18fd18dc0d
Felipe Maffezzolli <felipe.maffezzolli@gmail.com>
2019-01-20 17:53:52 -0200

    created class

---
+++ models/User.js
@@ -0,0 +1,11 @@
+class User {
+  constructor(name, gender, country, email, password, photo, admin) {
+    this.name = name;
+    this.gender = gender;
+    this.country = country;
+    this.email = email;
+    this.password = password;
+    this.photo = photo;
+    this.admin = admin;
+  }
+}
