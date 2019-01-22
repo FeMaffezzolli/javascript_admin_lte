9e50e472e52c824559fc833dfdffb78c27b8c350
Felipe Maffezzolli <felipe.maffezzolli@gmail.com>
2019-01-22 15:26:23 -0200

    fixed append child

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -80,18 +80,23 @@ class UserController {
   } // closing getValues()
 
   addUserLine(userData) {
-    this.tableEl.innerHTML = `
-      <tr>
-        <td><img src="${userData.photo}" alt="User Image" class="img-circle img-sm"></td>
-        <td>${userData.name}</td>
-        <td>${userData.email}</td>
-        <td>${userData.admin}</td>
-        <td>${userData.birth}</td>
-        <td>
-          <button type="button" class="btn btn-primary btn-xs btn-flat">Editar</button>
-          <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
-        </td>
-      </tr>
-    `;
+
+    let tr = document.createElement('tr');
+
+    tr.innerHTML = `
+    <tr>
+      <td><img src="${userData.photo}" alt="User Image" class="img-circle img-sm"></td>
+      <td>${userData.name}</td>
+      <td>${userData.email}</td>
+      <td>${userData.admin}</td>
+      <td>${userData.birth}</td>
+      <td>
+        <button type="button" class="btn btn-primary btn-xs btn-flat">Editar</button>
+        <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
+      </td>
+    </tr>
+  `;
+
+    this.tableEl.appendChild(tr)
   } // Closing addUserLine()
 }
